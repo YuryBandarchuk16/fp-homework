06382de61beb4d5dfd295ccc3d243bd7bea87d35
Yury Bandarchuk <ybandarchuk@celo.org>
2019-03-05 19:46:00 -0800

    Finish Task 4

--- hw0/src/Task4.hs
+++ hw0/src/Task4.hs
@@ -2,6 +2,9 @@ module Task4
     (
       fix
     , iterateElement
+    , fibonacci
+    , factorial
+    , mapFix
     ) where
 
 import Data.Function (fix)
@@ -9,10 +12,24 @@ import Data.Function (fix)
 iterateElement :: a -> [a]
 iterateElement x = fix (\l -> (x:l))
 
--- fibonacci :: Integer -> Integer
--- fibonacci n = fix fibonacciHelper (-1) 1 n where
---     fibonacciHelper _ cur next 0 = cur
---     fibonacciHelper f cur next n =
---         let newCur = next
---             newNext = cur + next in
---                  f newCur newNext
+fibonacci :: Integer -> Integer
+fibonacci = fix fibonacciHelper where
+    fibonacciHelper :: (Integer -> Integer) -> Integer -> Integer
+    fibonacciHelper f n = 
+        if n < 0 then error "fibonacci is not defined for negative numbers"
+        else if n <= 1 then n 
+        else f (n - 1) + f (n - 2)
+
+factorial :: Integer -> Integer
+factorial = fix factorialHelper where
+    factorialHelper :: (Integer -> Integer) -> Integer -> Integer
+    factorialHelper f n = 
+        if n < 0 then f (-n) * ((-1) ^ (n `mod` 2))
+        else if n <= 1 then 1
+        else n * f (n - 1)
+
+mapFix :: (a -> b) -> [a] -> [b]
+mapFix = fix mapHelper where
+    -- mapHelper :: (a -> b) -> [a] -> [b]
+    mapHelper _ _ [] = []
+    mapHelper f mapFunc (x:xs) = (mapFunc x):(f mapFunc xs)
